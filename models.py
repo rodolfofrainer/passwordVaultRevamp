93a0e7850d0ae9e7a9ff1376b4006633a231e8d8
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-06-08 12:40:09 +0100

    create login logic

--- models.py
+++ models.py
@@ -1,4 +1,10 @@
 from flask_login import UserMixin
+import bcrypt
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 class User(UserMixin):
@@ -14,6 +20,16 @@ class User(UserMixin):
         # return User.query.get(int(user_id))
         return None
 
+    def set_password(self, password):
+        # Hash, salt, and pepper the provided password
+        salt = bcrypt.gensalt()
+        pepper = os.environ.get("PEPPER").encode()
+        hashed_password = bcrypt.hashpw(password.encode(), salt + pepper)
+        self.password = hashed_password
+
     def check_password(self, password):
         # Compare the provided password with the stored password
-        return password == self.password
+        salt = bcrypt.gensalt()
+        pepper = os.environ.get("PEPPER").encode()
+        hashed_password = bcrypt.hashpw(password.encode(), salt + pepper)
+        return bcrypt.checkpw(password.encode(), hashed_password)
