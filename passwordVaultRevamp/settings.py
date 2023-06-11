73fff18a6997375515b05e184762d2ace340b36f
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-06-11 11:06:07 +0100

    install allauth and update requirements.txt

--- passwordVaultRevamp/settings.py
+++ passwordVaultRevamp/settings.py
@@ -37,11 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'vaultApp',
 
+    'allauth',
+    'allauth.account',
+
     'rest_framework',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- passwordVaultRevamp/urls.py
+++ passwordVaultRevamp/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('allauth.urls')),
 
     path("", include("vaultApp.urls")),
 ]
