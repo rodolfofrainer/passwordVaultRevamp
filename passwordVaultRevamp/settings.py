41605ae1fafb8c6bafde9c9fdbdb01477adfbc54
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-06-11 11:03:38 +0100

    create index page

--- passwordVaultRevamp/settings.py
+++ passwordVaultRevamp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'vaultApp',
+
     'rest_framework',
 ]
 
--- vaultApp/views.py
+++ vaultApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def my_view(request):
+    return render(request, 'vaultApp/index.html')
