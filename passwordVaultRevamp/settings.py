27b7be291e637b5402d73259eccda2f9c696b71c
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-06-13 11:13:56 +0100

    rearrange settings.py

--- passwordVaultRevamp/settings.py
+++ passwordVaultRevamp/settings.py
@@ -107,6 +107,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -153,10 +154,14 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+########## REST FRAMEWORK ##########
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
+########## CRISPY FORMS ##########
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+########## ALLAUTH ##########
+LOGIN_REDIRECT_URL = "/"
