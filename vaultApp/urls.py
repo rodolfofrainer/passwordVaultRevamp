15c3c31f2ab438d36c66d9f87bfe36e473ba9fc9
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-06-13 15:24:36 +0100

    remove redundant view

--- vaultApp/urls.py
+++ vaultApp/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("login/", views.login_view, name="login"),
     path("about/", views.about_view, name="about"),
 ]
--- vaultApp/views.py
+++ vaultApp/views.py
@@ -5,9 +5,5 @@ def index_view(request):
     return render(request, 'vaultApp/index.html')
 
 
-def login_view(request):
-    return render(request, 'vaultApp/login.html')
-
-
 def about_view(request):
     return render(request, 'vaultApp/about.html')
