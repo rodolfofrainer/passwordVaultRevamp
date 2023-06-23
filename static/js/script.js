fc5910fd52fb8ed5ee33034e4ae59b3b83f5d95d
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-06-23 12:47:20 +0100

    update password field for more security

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,16 @@ $('#confirmDeleteModal').on('show.bs.modal', function (event) {
     var actionUrl = form.attr('action').replace('0', vaultItemId);
     form.attr('action', actionUrl);
   });
+
+function togglePasswordVisibility(itemId) {
+    var passwordElement = document.getElementById("password" + itemId);
+    var passwordValue = passwordElement.getAttribute("data-password");
+
+    if (passwordElement.classList.contains('password-hidden')) {
+      passwordElement.classList.remove('password-hidden');
+      passwordElement.textContent = passwordValue;
+    } else {
+      passwordElement.classList.add('password-hidden');
+      passwordElement.textContent = '******';
+    }
+  }
