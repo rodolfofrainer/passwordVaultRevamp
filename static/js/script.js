8529ae70765b435392347573777f3788ed7788ab
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-06-23 13:23:01 +0100

    improve vault ui

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
-$("#confirmDeleteModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget);
-  var vaultItemId = button.data("vaultitemid");
-  var form = $("#deleteForm");
-  var actionUrl = form.attr("action").replace("0", vaultItemId);
-  form.attr("action", actionUrl);
+$(document).ready(function () {
+  $("#confirmDeleteModal").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget);
+    var vaultItemId = button.data("vaultitemid");
+    var form = $("#deleteForm");
+    var actionUrl = form.attr("action").replace("0", vaultItemId);
+    form.attr("action", actionUrl);
+  });
 });
 
 function togglePasswordVisibility(itemId) {
@@ -13,8 +15,7 @@ function togglePasswordVisibility(itemId) {
   if (passwordElement.classList.contains("password-hidden")) {
     passwordElement.classList.remove("password-hidden");
     passwordElement.textContent = passwordValue;
-    document.getElementById("copyButton" + itemId).style.display =
-      "inline-block";
+    document.getElementById("copyButton" + itemId).style.display = "inline-block";
   } else {
     passwordElement.classList.add("password-hidden");
     passwordElement.textContent = "******";
